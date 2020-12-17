414f303329dbca2ead2e3755580a3885c0620d82
Richard Enrico <richardenrico02@gmail.com>
2020-12-17 18:14:49 +0800

    .

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -36,6 +36,6 @@ class Filters extends BaseConfig
 	// that they should run on, like:
 	//    'isLoggedIn' => ['before' => ['account/*', 'profiles/*']],
 	public $filters = [
-		// 'login' => ['before' => ['admin', 'admin/*']]
+		'login' => ['before' => ['admin', 'admin/*']]
 	];
 }
